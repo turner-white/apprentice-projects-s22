7ed5027e267de2be189227d9ce83c8abf94f9d77
samrshi <samrshi@outlook.com>
2022-01-18 00:41:38 -0500

    Add mood tracker starter code

---
+++ p04-mood-tracker/p04-mood-tracker/p04_mood_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  p04_mood_trackerApp.swift
+//  p04-mood-tracker
+//
+//  Created by Samuel Shi on 1/17/22.
+//
+
+import SwiftUI
+
+@main
+struct p04_mood_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
