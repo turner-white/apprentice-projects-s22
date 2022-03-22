8bfa334d44613bfe343c680cabdcb503f53bbea0
Sam Shi <samrshi@outlook.com>
2022-03-22 17:50:23 -0400

    add corelocation demo

---
+++ ex04-corelocation/ex04-corelocation/ex04_corelocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ex04_corelocationApp.swift
+//  ex04-corelocation
+//
+//  Created by Samuel Shi on 3/22/22.
+//
+
+import SwiftUI
+
+@main
+struct ex04_corelocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
